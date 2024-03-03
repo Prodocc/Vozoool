6d55df768998f313ccc44e91ed498b5d84845325
Alexndeer <Alexandroff007@gmail.com>
2024-03-03 21:39:09 +0300

    Database implemented Created class User

--- src/main/java/com/vozoool/App00.java
+++ src/main/java/com/vozoool/App00.java
@@ -1,7 +1,29 @@
 package com.vozoool;
 
+import com.vozoool.entity.User;
+
+import java.sql.*;
+
 public class App00 {
-    public static void main(String[] args) {
-        System.out.println("...");
+    private static final String URL = "jdbc:mysql://localhost:3306/vozoool";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "12345";
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+        Statement statement = connection.createStatement();
+//        statement.executeUpdate("insert into users (user_id, user_name, user_surname, user_email" +
+//                ",user_password, user_type) values ('1', 'VlaDICK','Lyudoed','YaEmLyudei228@mail.ru','YaEmLyudei228','0');");
+        ResultSet set = statement.executeQuery("select * from users;");
+        while (set.next()) {
+            String user_name = set.getString("user_name");
+            String user_surname = set.getString("user_surname");
+            String user_email = set.getString("user_email");
+            String user_password = set.getString("user_password");
+            int user_type = set.getInt("user_type");
+            int user_id = set.getInt("user_id");
+            User user = new User(user_name, user_surname, user_email, user_password, user_type);
+            System.out.println(user);
+        }
     }
 }
